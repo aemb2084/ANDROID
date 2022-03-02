a2644d30462b90357f7adf096e7e198c5047e270
angel.manosalvas <angel.manosalvas@gmail.com>
2022-03-01 23:59:43 -0500

    Practice finished.

--- ViewModel/app/src/main/java/com/example/viewmodel/ui/main/MainFragment.kt
+++ ViewModel/app/src/main/java/com/example/viewmodel/ui/main/MainFragment.kt
@@ -1,12 +1,21 @@
 package com.example.viewmodel.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Toast
+import androidx.core.view.get
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.viewmodel.R
+import com.example.viewmodel.databinding.MainFragmentBinding
+import java.util.*
+
 
 class MainFragment : Fragment() {
 
@@ -14,19 +23,64 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+    private val TAG: String = "MainFragmentLogs"
+    private lateinit var _binding : MainFragmentBinding
+    private var num1 : Float = 0.0f
+    private var num2 : Float = 0.0f
+    val viewModel by viewModels<MainViewModel>()
+    private var mOperation : String = ""
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+
+        return  _binding.root
+
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val operations = resources.getStringArray(R.array.operations)
+        val adapter = ArrayAdapter(
+            requireContext(),
+            R.layout.list_item,
+            operations
+        )
+
+        _binding.opOptions.setOnItemClickListener { parent, view, position, id ->
+
+            mOperation = parent.getItemAtPosition(position).toString()
+            viewModel.chargeOper(mOperation)
+
+        }
+
+        (_binding.opOptions as? AutoCompleteTextView)?.setAdapter(adapter)
+
+        _binding.calculate.setOnClickListener{
+            val emptyChecker1 = _binding.number1.text.toString()
+            val emptyChecker2 = _binding.number2.text.toString()
+
+            if (emptyChecker1 != "" && emptyChecker2 != "" ){
+                num1 = _binding.number1.text.toString().toFloat()
+                num2 = _binding.number2.text.toString().toFloat()
+            }else{
+                Toast.makeText(requireContext(), "Please insert the numbers", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (mOperation != ""){
+                viewModel.doOper(num1, num2)
+            }else{
+                Toast.makeText(requireContext(), "Please select operation", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+        }
+
+        viewModel.operResult.observe(viewLifecycleOwner, androidx.lifecycle.Observer { result ->
+            _binding.operResult.text = result
+        })
+
     }
 
 }
